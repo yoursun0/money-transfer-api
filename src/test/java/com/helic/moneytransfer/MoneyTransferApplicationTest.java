c5e49f68c3fd42bae969ee290129134b7eec8682
Helic Leung <yoursun0@gmail.com>
2018-11-05 14:56:21 +0800

    Add banner.txt, main application and test

---
+++ src/test/java/com/helic/moneytransfer/MoneyTransferApplicationTest.java
@@ -0,0 +1,16 @@
+package com.helic.moneytransfer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoneyTransferApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
